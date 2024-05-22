ad195cfa294731e3852358eded3a56ee760bc033
Anuj Tripathi <anujtr455@gmail.com>
2024-05-22 14:18:27 +0530

    pri

--- script.js
+++ script.js
@@ -10,6 +10,8 @@ const gencompChoice = () => {
     const options = ["rock", "paper", "scissors"];
     let randidx = Math.floor(Math.random() * 3);
     return options[randidx];
+
+    
 };
 
 const drawGame = () => {
